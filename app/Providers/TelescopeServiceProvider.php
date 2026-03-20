6d9d12d536bfc2d942ff2e59cf94a2ca1160cd45
Steve Callan <sjcallan@gmail.com>
2026-03-20 16:40:55 -0400

    Allow me to see telescope

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -58,7 +58,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     {
         Gate::define('viewTelescope', function (User $user) {
             return in_array($user->email, [
-                //
+                'sjcallan@gmail.com'
             ]);
         });
     }
